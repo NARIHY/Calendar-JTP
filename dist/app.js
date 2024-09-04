2c6360cf6339a08400f028d1400bcf805bdbb5b5
NARIHY <maheninarandrianarisoa@gmail.com>
2024-09-04 13:38:26 +0300

    Correction bug + fusion

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//app.ts
-const calendar_1 = require("./calendar");
-//initialize the calendar when document is ready
-document.addEventListener('DOMContentLoaded', () => {
-    new calendar_1.Calendar();
-});
--- readme.md
+++
@@ -1,4 +0,0 @@
-là j'arrive pas a avoir mon calendrier, il y en a un gros bug, je sais pas il n'arrive pas a exporter le calendrier
-
-
-Projet en cours.....
